ba045b3c24f679312910721050791e629e223503
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-22 16:04:24 +0100

    Update index.php

--- accessi.cookie-master/web/index.php
+++ accessi.cookie-master/web/index.php
@@ -1,22 +1,17 @@
 <html> 
     <head> 
-        <title>Contatore accessi</title> 
+        <title>Visualizzazione Array Associativo</title> 
    </head> 
   <body> 
-<?php 
-    if (!isset($_COOKIE['count']))
-    {
-        echo "Benvenuto! Questa e' la prima volta che accedi a questo sito!";
-        $cookie = 1;
-        setcookie("count", $cookie,time()+60*60*24*7);
-    }
-    else
-    {
-        $cookie = $_COOKIE['count']+1;
-        echo "Hai visitato questa pagina $cookie volte."; 
-        setcookie("count", $cookie);
-    }  
-      ?> 
+        < table border = "1" >
+            <tr ><th > Personaggio </ th > </ tr >
+            <? php
+            $pers = array (" pippo " ," pluto " ," paperino " ," topolino " );
+            foreach ( $pers as $nome ) {
+            print " <tr ><td > $nome </ td > </ tr >\ n";
+            }
+            ?>
+        </ table >
    </body> 
 </html>
 
