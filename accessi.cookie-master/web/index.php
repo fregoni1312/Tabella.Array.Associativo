d26c4aa48abad1e2416f01c6da448adbf7d8a749
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-11-22 16:17:58 +0100

    Update index.php

--- accessi.cookie-master/web/index.php
+++ accessi.cookie-master/web/index.php
@@ -1,21 +1,15 @@
-<html> 
-    <head> 
-        <title>Visualizzazione Array Associativo</title> 
-   </head> 
-    <body> 
-  < table border = "1" >
-<tr ><th > Chiave </ th >< th > Personaggio </ th > </ tr >
-<? php
-$pers = array ("a "=>" pippo " ,"b" =>" pluto " ,
-"c" =>" paperino " ," d"=>" topolino " );
 
-foreach ( $pers as $idx = > $nome ) {
-print " <tr ><td > $idx </ td ><td > $nome </ td > </ tr >\ n ";
-}
-?>
-</ table >
-   </body> 
-</html>
+            < table border = "1" >
+            <tr ><th > Chiave </ th >< th > Personaggio </ th > </ tr >
+            <? php
+            $pers = array ("a "=>" pippo " ,"b" =>" pluto " ,
+            "c" =>" paperino " ," d"=>" topolino " );
+
+            foreach ( $pers as $idx = > $nome ) {
+            print " <tr ><td > $idx </ td ><td > $nome </ td > </ tr >\ n ";
+            }
+            ?>
+            </ table >
 
 
 
